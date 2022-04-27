abc27b62e308801f1d26e2ec11c7ca6f9d7d0eba
Mayank Gangwar <73337152+Mayankgangwr@users.noreply.github.com>
2022-04-27 15:11:56 +0530

    Add files via upload

---
+++ Array/Margesort.java
@@ -0,0 +1,79 @@
+public class Margesort {
+    static void merge(int arr[], int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        /* Copy data to temp arrays */
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    static void diveder(int arr[], int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            diveder(arr, l, m);
+            diveder(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+
+    }
+
+    static void printarr(int aa[]) {
+        for (int i = 0; i < aa.length; i++) {
+            System.out.print(aa[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 1, 7, 2, 3, 4, 9, 5 };
+        int n = arr.length;
+        int l = 0;
+        int r = n - 1;
+        diveder(arr, l, r);
+        printarr(arr);
+    }
+}
